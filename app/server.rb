254ee564c03b18f9f29d566c034cf2c2982fdbee
redstone2010 <31410740+redstone2010@users.noreply.github.com>
2017-10-29 08:40:32 -0400

    Update server.rb

--- app/server.rb
+++ app/server.rb
@@ -1,1 +1,7 @@
-
+def pullable_by?(user)
+  science "repository.pullable-by" do |experiment|
+    experiment.context :repo => id, :user => user.id
+    experiment.use { is_collaborator?(user) }
+    experiment.try { has_access?(user) }
+  end
+end

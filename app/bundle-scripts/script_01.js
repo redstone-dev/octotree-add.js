e053fcde324eca29a7598855e8429337ada6971f
redstone2010 <31410740+redstone2010@users.noreply.github.com>
2017-10-29 08:54:13 -0400

    Update script_01.js

--- app/bundle-scripts/script_01.js
+++ app/bundle-scripts/script_01.js
@@ -1,1 +1,10 @@
-
+function init(configType, colour, bgColour){
+  var ghConfig = {
+    _config: configType,
+    _colour: colour,
+    _bg: bgColour,
+    _defaults: ["_config: configure", 
+    "_colour: white", 
+     "_bg: black" ] 
+  };
+}
